3df241f926e9e33efa450f4e5533d84c24e647ae
Emanuel <emanuel.morille@proton.me>
2022-12-08 12:31:16 +0100

    :art: Improved start.sh

--- start.sh
+++ start.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
 configfolder=$HOME/.config/maobot
-logfile=$configfolder/log.txt
+logfile=$configfolder/maobot.log
 pidfile=$configfolder/maobot.pid
 
 function store_pid() {
-	echo "$1" > "$pidfile"
+    echo "$1" >"$pidfile"
 }
 
 function kill_previous_process() {
-	if test -f "$pidfile"; then
-		pid=$(cat "$pidfile" 2>/dev/null)
-		kill "$pid" &>/dev/null
-		rm "$pidfile" 2>/dev/null
-	fi
+    if test -f "$pidfile"; then
+        pid=$(cat "$pidfile" 2>/dev/null)
+        kill "$pid" &>/dev/null
+        rm "$pidfile" 2>/dev/null
+    fi
 }
 
 function start() {
-  mkdir -p "$configfolder"
-  kill_previous_process
-  nohup python3 main.py &> "$logfile" &  
-  store_pid "$!" 
+    mkdir -p "$configfolder"
+    kill_previous_process
+    nohup python3 main.py >>"$logfile" &
+    store_pid "$!"
 }
 
 start
-
