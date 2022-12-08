a680b68471ea2c7005fcd360824faca8f0a5b6ed
Emanuel <emanuel.morille@proton.me>
2022-12-08 17:53:33 +0100

    :art: 'règle' command now allows rerolling

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import time
 import random
 import discord
 from discord.ext.commands import Context
@@ -8,8 +9,22 @@ load_dotenv()
 bot = discord.Bot()
 
 
+def format_rule(rule: str) -> str:
+    words = rule.split("-")
+    formatted_rule = ""
+
+    for word in words:
+        if word in ["d", "l"]:
+            formatted_rule += word + "'"
+        else:
+            formatted_rule += word + " "
+
+    return formatted_rule.capitalize()
+
+
 @bot.event
 async def on_ready():
+    print(f"{time.strftime('[%d-%m-%Y %H:%M:%S]',time.localtime())}")
     print(f"{bot.user} is ready and online!")
 
 
@@ -18,29 +33,40 @@ async def on_ready():
     description="Choisi une règle au hasard parmi toutes celles existantes",
 )
 async def regle(ctx: Context):
-    def format_rule(rule: str) -> str:
-        words = rule.split("-")
-        formatted_rule = ""
-
-        for word in words:
-            if word in ["d", "l"]:
-                formatted_rule += word + "'"
-            else:
-                formatted_rule += word + " "
-
-        return formatted_rule.capitalize()
-
-    rules = set(
-        channel.name
-        for channel in ctx.guild.channels
-        if channel.name.startswith("règle-")
+    rules = list(
+        set(
+            channel.name
+            for channel in ctx.guild.channels
+            if channel.name.startswith("règle-") and channel.name != "règle-de"
+        )
     )
-    chosen_rule = random.choice(tuple(rules))
 
-    embed = discord.Embed()
-    embed.add_field(name="La règle choisie est... 🃏", value=format_rule(chosen_rule))
+    def generate_embed() -> discord.Embed:
+        chosen_rule = rules.pop(random.randrange(len(rules)))
 
-    await ctx.respond(embed=embed)
+        embed = discord.Embed()
+        embed.add_field(
+            name="La règle choisie est... 🃏", value=format_rule(chosen_rule)
+        )
+
+        return embed
+
+    def generate_view() -> discord.ui.View:
+        reroll_button = discord.ui.Button(label="Reroll")
+        reroll_button.callback = reroll
+
+        return discord.ui.View(reroll_button)
+
+    async def reroll(interaction: discord.Interaction):
+        if interaction.user == ctx.author:
+            if len(rules) > 1:
+                await interaction.response.edit_message(embed=generate_embed())
+            else:
+                await interaction.response.edit_message(
+                    embed=generate_embed(), view=None
+                )
+
+    await ctx.respond(embed=generate_embed(), view=generate_view())
 
 
 bot.run(os.getenv("TOKEN"))
